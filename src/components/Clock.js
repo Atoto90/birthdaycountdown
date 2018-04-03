837dc0493bf974b35880b7346ec97e4e42666af6
Oscar A Infante <atoto90@Oscars-MacBook-Pro.local>
2018-04-03 16:09:42 -0600

    fixed same day case

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -36,6 +36,8 @@ class Clock extends Component {
             }
             else if(birthDay < currentDay) {
                 bday.setFullYear(today.getFullYear() + 1);
+            } else if(birthDay == currentDay) {
+                return 0
             }
         }
 
@@ -82,15 +84,22 @@ class Clock extends Component {
 
         return (
             <div>
-                <div>
-                    <div>DAYS {data.days}</div>
-                    <div>HRS {data.hours}</div>
-                    <div>MINS {data.minutes}</div>
-                    <div>SECS {data.seconds}</div>
-                </div>
-                <div>
-                    {<h4>remanining until you are {this.getAge()}</h4>}
-                </div>
+                {
+                    this.state.timeRemaining == 0 ?
+                        <h1>Happy Birthday!</h1>
+                        :
+                        <div>
+                            <div>
+                                <div>DAYS {data.days}</div>
+                                <div>HRS {data.hours}</div>
+                                <div>MINS {data.minutes}</div>
+                                <div>SECS {data.seconds}</div>
+                            </div>
+                            <div>
+                                {<h4>remanining until you are {this.getAge()}</h4>}
+                            </div>
+                        </div>    
+                }
             </div>
         )
     }
